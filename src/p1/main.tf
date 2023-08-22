e80a44d5a00e861ba2ee78b0dac552c5d8990269
daviddong091216 <daviddong201712@gmail.com>
2023-08-22 09:53:01 -0500

    20230822

--- src/p1/main.tf
+++ src/p1/main.tf
@@ -1,4 +1,15 @@
+
+terraform {
+  required_providers {
+    local = {
+      source = "hashicorp/local"
+      version = "2.2.2"
+    }
+  }
+}
+
+
 resource "local_file" "pet" {
   filename="C:/0-1-dtgwork/training/Terraform/terraformPractice/pet.txt"
-  content="We love pets!"
+  content="We really love pets!"
 }
