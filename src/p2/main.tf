e426304b251af3a3671e24d8d0cd41298d2f976a
daviddong091216 <daviddong201712@gmail.com>
2023-08-23 09:46:43 -0500

    20230823

--- src/p2/main.tf
+++ src/p2/main.tf
@@ -1,9 +1,13 @@
-resource "local_file" "t1" {
-  filename = "C:/0-1-dtgwork/training/Terraform/terraformPractice/src/p2/t1.txt"
+resource "local_file" "t21" {
+  filename = "C:/0-1-dtgwork/training/Terraform/terraformPractice/src/p2/t21.txt"
   content  = "We really love pets!"
 }
 
-resource "local_file" "t2" {
+resource "local_file" "t22" {
   filename = var.filename
   content  = var.content
+}
+
+output "t22-content"{
+  value = local_file.t22.content
 }
--- src/p2/variables.tf
+++ src/p2/variables.tf
@@ -1,6 +1,10 @@
 variable "filename" {
-  default = "C:/0-1-dtgwork/training/Terraform/terraformPractice/src/p2/t2.txt"
+  default = "C:/0-1-dtgwork/training/Terraform/terraformPractice/src/p2/t22.txt"
 }
 variable "content" {
-  default = "It is for t2.txt"
+  default = "It is for t22.txt"
+}
+variable "region" {
+  type = list
+  default=["us-east-1","us-east-1","us-east-2"]
 }
